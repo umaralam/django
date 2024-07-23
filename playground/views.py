fc6bbd99d87321fa8a9f88cf4e733728691a8ed1
Md Umar Alam <alam@onmobile.com>
2024-07-23 20:21:43 +0530

    mysql database creation

--- playground/views.py
+++ playground/views.py
@@ -7,4 +7,4 @@ from django.http import HttpResponse
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'ame': 'Md Umar Alam'})
+    return render(request, 'hello.html', {'name': 'Md Umar Alam'})
